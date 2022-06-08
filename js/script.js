2e38e9b9f5795c25e007b9cbc1ce545672935037
Lucas Minatelli Rezende <lucas.minatelli@gmail.com>
2022-06-08 18:07:21 -0300

    mario playing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
-var mario = document.querySelector(".mario");
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
 
 const jump = () => {
   mario.classList.add("jump");
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
 };
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const cloudsPosition = clouds.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+    mario.src = "../assets/game_over.png";
+    mario.style.width = "70px";
+    mario.style.marginLeft = "50px";
+
+    clouds.style.animation = "none";
+    clouds.style.left = `${cloudsPosition}px`;
+
+    clearInterval(loop);
+  }
+}, 10);
 
 document.addEventListener("keydown", jump);
